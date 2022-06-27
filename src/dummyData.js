03b07958550c3c154de8c39f68ef8e190ed6523a
Eyob <ateyob@gmail.com>
2022-06-27 10:23:47 +0300

    edit user

---
+++ src/dummyData.js
@@ -0,0 +1,86 @@
+export const userData = [
+    {
+      name: "Jan",
+      "Active User": 4000,
+    },
+    {
+      name: "Feb",
+      "Active User": 3000,
+    },
+    {
+      name: "Mar",
+      "Active User": 5000,
+    },
+    {
+      name: "Apr",
+      "Active User": 4000,
+    },
+    {
+      name: "May",
+      "Active User": 3000,
+    },
+    {
+      name: "Jun",
+      "Active User": 2000,
+    },
+    {
+      name: "Jul",
+      "Active User": 4000,
+    },
+    {
+      name: "Agu",
+      "Active User": 3000,
+    },
+    {
+      name: "Sep",
+      "Active User": 4000,
+    },
+    {
+      name: "Oct",
+      "Active User": 1000,
+    },
+    {
+      name: "Nov",
+      "Active User": 4000,
+    },
+    {
+      name: "Dec",
+      "Active User": 3000,
+    },
+  ];
+
+  export const productData = [
+    {
+      name: "Jan",
+      "Sales": 4000,
+    },
+    {
+      name: "Feb",
+      "Sales": 3000,
+    },
+    {
+      name: "Mar",
+      "Sales": 5000,
+    },
+  ];
+
+  
+  export const userRows = [
+    { id: 1, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
+    { id: 2, lastName: 'Snow', firstName: 'Jon', avatar:"/images/pic4.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
+    { id: 3, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
+    { id: 4, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
+    { id: 5, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
+    { id: 6, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
+    { id: 7, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
+    { id: 8, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
+    { id: 9, lastName: 'Snow', firstName: 'Jon', avatar:"/images/pic1.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
+    { id: 10, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
+    { id: 11, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
+    { id: 12, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
+    { id: 13, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
+    { id: 14, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
+    { id: 15, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
+    { id: 16, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
+    { id: 17, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
+  ];
--- src/pages/userList/UserList.jsx
+++ src/pages/userList/UserList.jsx
@@ -1,9 +1,17 @@
 import "./userList.css"
 import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import {DeleteOutline} from '@mui/icons-material';
+import {Link} from 'react-router-dom';
+import {userRows} from "../../dummyData";
+import { useState } from "react";
 
+export default function UserList() {
 
-export default function userList() {
+  const [data, setData] = useState(userRows);
+
+  const handleDelete = (id) => {
+    setData(data.filter((item) => item.id !== id));
+  };
 
   const columns: GridColDef[] = [
     { field: 'id', headerName: 'ID', width: 100 },
@@ -34,7 +42,9 @@ export default function userList() {
       renderCell: (params) => {
         return(
           <>
-          <button className="userListEdit">Edit</button>
+          <Link to={"/user/"+params.id}>
+              <button className="userListEdit">Edit</button>
+          </Link>
           <DeleteOutline className="userListDelete" />
           </>
         );
@@ -42,28 +52,10 @@ export default function userList() {
     },
   ];
   
-  const rows = [
-    { id: 1, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
-    { id: 2, lastName: 'Snow', firstName: 'Jon', avatar:"/images/pic4.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
-    { id: 3, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
-    { id: 4, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
-    { id: 5, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
-    { id: 6, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
-    { id: 7, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
-    { id: 8, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
-    { id: 9, lastName: 'Snow', firstName: 'Jon', avatar:"/images/pic1.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
-    { id: 10, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
-    { id: 11, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
-    { id: 12, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
-    { id: 13, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
-    { id: 14, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
-    { id: 15, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
-    { id: 16, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
-    { id: 17, lastName: 'Snow', firstName: 'Jon', avatar:"/images/smiley.jpg" , phoneNum: +251921567692, totalSpent: '255 ETB' },
-  ];
+  
   return (
     <div className="userList">
-        <DataGrid rows={rows} disableSelectionOnClick columns={columns} pageSize={8} rowsPerPageOptions={[5]} checkboxSelection />
+        <DataGrid rows={data} disableSelectionOnClick columns={columns} pageSize={8} rowsPerPageOptions={[5]} checkboxSelection />
     </div>
   )
 }
